463082331a9820eaff70ccd782d17fce43f5e107
babinskajustyna <babinskajustynaa@gmail.com>
2019-04-02 13:10:42 +0200

    Basic/Upper, Screenshots

--- src/main/java/pl/justynababinska/formatter/BasicTextFormatter.java
+++ src/main/java/pl/justynababinska/formatter/BasicTextFormatter.java
@@ -1,8 +1,10 @@
 package pl.justynababinska.formatter;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BasicTextFormatter implements TextFormatter{
 
 	@Override
--- src/main/java/pl/justynababinska/formatter/UpperCaseTextFormatter.java
+++ src/main/java/pl/justynababinska/formatter/UpperCaseTextFormatter.java
@@ -1,10 +1,8 @@
 package pl.justynababinska.formatter;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class UpperCaseTextFormatter implements TextFormatter{
 
 	@Override
